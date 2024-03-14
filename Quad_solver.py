c9122a351b38a99eb2a8d337a46b849b63ddfcc8
Redpandas24 <163441124+Redpandas24@users.noreply.github.com>
2024-03-14 09:15:37 -0700

    Add files via upload
    A code that solves the quadratic equation. Doesn't imply imaginary numbers.

---
+++ Quad_solver.py
@@ -0,0 +1,19 @@
+import math 
+     
+print('I will solve the equation ax^2+bx+c=o') 
+     
+a = int(input('a= ')) 
+b = int(input('b= ')) 
+c = int(input('c= ')) 
+     
+d = b**2-4*a*c 
+     
+if d < 0: 
+   print('The equation has no real solution') 
+elif d == 0: 
+   x = (-b)/(2*a) 
+   print('This equation has one solution: ',x) 
+else: 
+   x1 = (-b+math.sqrt(d))/(2*a) 
+   x2 = (-b-math.sqrt(d))/(2*a) 
+   print('This equation has two solutions: ',x1, 'or', x2) 
